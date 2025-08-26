b249c369e98fd1557575e556669e7ea386ed3561
bittner <bittner@irz.uni-hannover.de>
2025-08-26 10:38:55 +0200

    Add .gitingore to ignore slurm .out and .err files. Add slurm shell script.

---
+++ dynamic_analysis/Model_1/wind_dyn.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=wind-dyn
+#SBATCH --mail-user=bittner@irz.uni-hannover.de
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output=uq_model_%j.out     # Output file
+#SBATCH --error=uq_model_%j.err      # Error file
+#SBATCH --ntasks=40                  # Number of tasks (adjust as needed)
+
+# Navigate to the working directory
+cd $SLURM_SUBMIT_DIR
+
+module load OpenSees
+
+# Run Julia script with parallel execution
+julia --project=. --threads=$SLURM_NTASKS dynamic_analysis_1.jl
